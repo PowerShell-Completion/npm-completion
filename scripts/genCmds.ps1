8e73de7500df630d3ee3c2ffcbe992703c4c53d3
Saran Tanpituckpong <sarunta@gmail.com>
2019-03-31 19:33:16 +0700

    :recycle: Simplify script

--- scripts/genCmds.ps1
+++ scripts/genCmds.ps1
@@ -1,16 +1,8 @@
 $srcCommandPath = Join-Path $PSScriptRoot '../resources/commands' -Resolve
 $destCommandPath = [System.IO.Path]::GetFullPath((Join-Path $PSScriptRoot '../src/commands.ps1'))
 
-$rawCommands = Get-Content $srcCommandPath | Out-String
-$commands = $rawCommands -split '\r\n|\n' | ForEach-Object {
-	$trimmedCommand = $_.Trim()
-
-	if ($trimmedCommand.Length -eq 0) {
-		return
-	}
-
-	return $trimmedCommand
-}
+$rawCommands = (Get-Content $srcCommandPath | Out-String).Trim()
+$commands = $rawCommands -split '\r\n|\n' | ForEach-Object { $_.Trim() }
 $commandsStr = ($commands | ForEach-Object { "`t'$_'" }) -join ",`n"
 $commandsScriptContent = "`$cmds = @(`n$commandsStr`n)"
 

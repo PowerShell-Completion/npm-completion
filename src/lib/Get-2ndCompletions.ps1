fd1cce3e67e73700a5834015872fb4e1dbb71b10
Saran Tanpituckpong <sarunta@gmail.com>
2020-10-29 01:06:31 +0700

    ✨ Add script completion

--- src/lib/Get-2ndCompletions.ps1
+++ src/lib/Get-2ndCompletions.ps1
@@ -5,6 +5,16 @@
 function Get-2ndCompletions([string] $WordToComplete, [string] $1stPart) {
 	$searchBlock = { $_ -like "$WordToComplete*" }
 	$completions = [System.Management.Automation.CompletionResult[]] @()
+
+	if (($1stPart -eq 'run-script') -or ($1stPart -eq 'run')) {
+		$scripts = Get-PackageScripts
+		$completions += $scripts | Where-Object $searchBlock | ForEach-Object {
+			return [System.Management.Automation.CompletionResult]::new($_, $_, 'Keyword', $_)
+		}
+
+		return $completions
+	}
+
 	$commandInfo = $COMMANDS[$1stPart]
 	$subCommands = $commandInfo.SubCommands | ForEach-Object {
 		return $_.Name
